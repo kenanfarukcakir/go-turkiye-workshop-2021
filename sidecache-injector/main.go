76c926e4becafe994ef46709d5b7072d4abb44ff
Kenan Faruk Çakır <kenanfarukcakir@gmail.com>
2021-12-19 14:32:37 +0300

    sidecache-injector and k8s resources

---
+++ sidecache-injector/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"sidecache-injector/pkg/mutators"
+
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/api/extensions/v1beta1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+
+	"net/http"
+
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+var (
+	runtimeScheme = runtime.NewScheme()
+	codecs        = serializer.NewCodecFactory(runtimeScheme)
+	deserializer  = codecs.UniversalDeserializer()
+
+	// (https://github.com/kubernetes/kubernetes/issues/57982)
+	defaulter = runtime.ObjectDefaulter(runtimeScheme)
+	version   string
+)
+
+func init() {
+	_ = corev1.AddToScheme(runtimeScheme)
+	_ = admissionregistrationv1beta1.AddToScheme(runtimeScheme)
+	_ = admissionregistrationv1.AddToScheme(runtimeScheme)
+	_ = v1.AddToScheme(runtimeScheme)
+}
+
+func main() {
+
+	webhookTlsCert, _ := tls.LoadX509KeyPair("/tmp/tls-secret/tls.crt", "/tmp/tls-secret/tls.key")
+
+	server := &http.Server{
+		Addr:      ":8080",
+		TLSConfig: &tls.Config{Certificates: []tls.Certificate{webhookTlsCert}},
+	}
+
+	sidecacheWebHookPath := "/mutate"
+
+	sidecacheMutatingHook := &admission.Webhook{
+		Handler: admission.HandlerFunc(func(ctx context.Context, req admission.Request) admission.Response {
+			fmt.Println("Webhook mutate endpoint...")
+			decoder, _ := admission.NewDecoder(runtimeScheme)
+			var deploymentObject v1beta1.Deployment
+			err := decoder.Decode(req, &deploymentObject)
+			if err != nil {
+				fmt.Println(err)
+				return admission.Errored(http.StatusInternalServerError, err)
+			}
+
+			fmt.Println("Decoded deployment object...")
+
+			return mutators.InjectSidecache(req, deploymentObject)
+		}),
+	}
+
+	pepSidecarHookHandler, _ := admission.StandaloneWebhook(sidecacheMutatingHook, admission.StandaloneOptions{})
+
+	http.Handle(sidecacheWebHookPath, pepSidecarHookHandler)
+
+	server.ListenAndServeTLS("", "")
+}
---
+++ sidecache-injector/pkg/mutators/mutator.go
@@ -0,0 +1,84 @@
+package mutators
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	"net/http"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/api/extensions/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+var (
+	sidecacheAnnotationInjectKey = "sidecache.trendyol.com/inject"
+
+	limitMemory, _   = resource.ParseQuantity("1Gi")
+	limitCPU, _      = resource.ParseQuantity("1")
+	requestCPU, _    = resource.ParseQuantity("30m")
+	requestMemory, _ = resource.ParseQuantity("30Mi")
+)
+
+func InjectSidecache(req admission.Request, deploymentObject v1beta1.Deployment) admission.Response {
+	if deploymentObject.Spec.Template.ObjectMeta.Annotations[sidecacheAnnotationInjectKey] != "true" {
+		for i, c := range deploymentObject.Spec.Template.Spec.Containers {
+			if c.Name == "sidecache" {
+				deploymentObject.Spec.Template.Spec.Containers = append(deploymentObject.Spec.Template.Spec.Containers[:i], deploymentObject.Spec.Template.Spec.Containers[i+1:]...)
+				fmt.Println("Removing sidecache...")
+			}
+		}
+	} else {
+		alreadyExists := false
+		for _, c := range deploymentObject.Spec.Template.Spec.Containers {
+			if c.Name == "sidecache" {
+				alreadyExists = true
+			}
+		}
+
+		if !alreadyExists {
+			currentAnnotations := deploymentObject.Spec.Template.ObjectMeta.Annotations
+			sidecache := createSidecacheContainer(currentAnnotations)
+			deploymentObject.Spec.Template.Spec.Containers = append(deploymentObject.Spec.Template.Spec.Containers, sidecache)
+			fmt.Println("Injecting sidecache...")
+		}
+	}
+
+	marshaledObj, err := json.Marshal(deploymentObject)
+	if err != nil {
+		fmt.Println(err)
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+
+	fmt.Println("Sending kubeapiserver admissionResponse...")
+	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledObj)
+}
+
+func createSidecacheContainer(currentAnnotations map[string]string) corev1.Container {
+	sidecacheImage := os.Getenv("SIDECACHE_IMAGE")
+
+	return corev1.Container{
+		Name:  "sidecache",
+		Image: sidecacheImage,
+		Resources: corev1.ResourceRequirements{
+			Limits: corev1.ResourceList{
+				corev1.ResourceCPU:    limitCPU,
+				corev1.ResourceMemory: limitMemory,
+			},
+			Requests: corev1.ResourceList{
+				corev1.ResourceCPU:    requestCPU,
+				corev1.ResourceMemory: requestMemory,
+			},
+		},
+		ImagePullPolicy: corev1.PullAlways,
+		Env: []corev1.EnvVar{
+			{
+				Name:  "EXAMPLE_ENV_VAR",
+				Value: "example_env_var",
+			},
+		},
+	}
+}

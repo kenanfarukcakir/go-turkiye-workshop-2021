88087398ca832b94090eaef8d9ce37c1da4a0496
Kenan Faruk Çakır <kenanfarukcakir@gmail.com>
2021-12-19 14:50:44 +0300

    demo-api

---
+++ demo-api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Endpoint     string `json:"endpoint"`
+	Year         int    `json:"year"`
+	Organization string `json:"organization"`
+}
+
+func cache(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("cachable", "ttl=30")
+
+	response := Response{
+		Endpoint:     "Cachable",
+		Year:         2021,
+		Organization: "GoTurkiye",
+	}
+
+	b, _ := json.Marshal(response)
+	w.Write(b)
+
+}
+
+func noCache(w http.ResponseWriter, r *http.Request) {
+
+	response := Response{
+		Endpoint:     "NOT cachable",
+		Year:         2021,
+		Organization: "GoTurkiye",
+	}
+
+	b, _ := json.Marshal(response)
+	w.Write(b)
+}
+
+func main() {
+
+	http.HandleFunc("/cache", cache)
+	http.HandleFunc("/no-cache", noCache)
+
+	http.ListenAndServe(":8080", nil)
+
+}
